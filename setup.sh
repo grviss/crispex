14601fd51c417471008930800ed909aac792f088
Gregal Vissers <gregal.vissers@astro.su.se>
2020-10-02 09:54:59 +0200

    Simplify extraction of last commit info

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@
 # Get the version information accessible to crispex
 
 git_id=`git describe --tags --long`
-last_commit_date=`git log -1 --date=format:"%Y/%m/%d %T" --format="%ad"`
-last_commit_author=`git log -1 --format="%an"`
+last_commit=`git log -1 --date=format:"%Y/%m/%d %T" --format="%ad %an"`
 
-echo "$git_id ($last_commit_date $last_commit_author)" > resources/versioninfo.txt
+echo "$git_id ($last_commit)" > resources/versioninfo.txt
 echo "crispex setup: done!"
